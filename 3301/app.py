b4d5a8e90312512d423031f3082a906308e0f6ce
Dan12971 <tedlugen@gmail.com>
2025-06-29 18:59:21 -0400

    Update app.py

--- 3301/app.py
+++ 3301/app.py
@@ -87,3 +87,4 @@ def get_address_info(address): return jsonify(blockchain.get_address_data(addres
 if __name__ == '__main__':
     parser = ArgumentParser(); parser.add_argument('-p', '--port', default=5000, type=int, help='port to listen on'); args = parser.parse_args()
     app.run(host='0.0.0.0', port=args.port)
+    
